e8ea0f151a9a3dd671a45e5a158536bace3ebede
Eric Parsons <eric.parsons@vertigis.com>
2024-03-30 16:29:52 -0600

    Some change to nvim key bindings.

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -11,6 +11,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- demon so be careful!
 vim.cmd([[let g:netrw_bufsettings = "noma nomod nu nobl nowrap ro"]])
 
+-- Whole file search and replace.
+vim.keymap.set("n", "<Leader>rp", ":%s//g<Left><Left>")
+
 -- Keymaps for better default experience.
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
@@ -18,13 +21,16 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Make it easier to move around windows. The key <c-l> has existing
--- functionality, but is a bit obscure (redraws and clears search
--- highlights).
+-- Make it easier to move around windows.
 vim.keymap.set("n", "<c-h>", "<c-w>h")
 vim.keymap.set("n", "<c-j>", "<c-w>j")
 vim.keymap.set("n", "<c-k>", "<c-w>k")
 vim.keymap.set("n", "<c-l>", "<c-w>l")
+
+vim.keymap.set("n", "<Leader><Left>", "<c-w>h")
+vim.keymap.set("n", "<Leader><Down>", "<c-w>j")
+vim.keymap.set("n", "<Leader><Up>", "<c-w>k")
+vim.keymap.set("n", "<Leader><Right>", "<c-w>l")
 
 -- Diagnostic keymaps.
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
