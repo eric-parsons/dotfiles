8f5b5ac8e0648cb5b712089f6af9adb706023ee0
Eric Parsons <eric.d.parsons@protonmail.com>
2024-11-27 00:49:33 -0600

    Minor updates to nvim keybindings.

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,6 +1,14 @@
 -- NOTE: See also the config for LSP, file tree, and Telescope which also add
 -- keymaps.
 
+-- NOTE: I use a programmable keyboard that lets me use actual arrow keys, pg
+-- up/down, etc. without moving my hand from home row. I also use Colemak
+-- rather than QWERTY layout. Thus these bindings are biased in favor of using
+-- navigation keys over HJKL. There are still some bindings for the latter in
+-- case I am stuck without my keyboard but they are there more as an
+-- afterthought. If you ase using HJKL, you will likely need to adjust some of
+-- these.
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
@@ -13,6 +21,12 @@ vim.g.maplocalleader = " "
 keymap.set({ "n", "v" }, "<space>", "<Nop>", opts)
 
 -- ((( General )))
+
+-- Make the Home key behave like "^", i.e. move to the first nonblank character
+-- on a line. The default behavior is the equivalent of "0" where it ignores
+-- indentation and moves to the very beginning of the line, which is less
+-- useful in the vast majority of cases.
+keymap.set({ "n", "v" }, "<home>", "^", opts)
 
 -- Make common commands a bit faster. Saving one or two characters doesn't seem
 -- like a big deal, but these are used a LOT. Also, about 10% of the time that
@@ -27,8 +41,8 @@ keymap.set("n", "<leader>W", "<cmd>wa<cr>", opts)
 opts.desc = "Close window"
 keymap.set("n", "<leader>q", "<cmd>q<cr>", opts)
 
-opts.desc = "Write all bufeers and close all windows"
-keymap.set("n", "<leader>Q", "<cmd>wqa<cr>", opts)
+opts.desc = "Write all buffers and quit Neovim"
+keymap.set("n", "<leader>Q", "<cmd>xa<cr>", opts)
 
 -- Move highlighted lines up/down.
 opts.desc = "Move selection down one line"
@@ -39,10 +53,6 @@ opts.desc = "Move selection up one line"
 keymap.set("v", "K", ":m '<-2<cr>gv=gv", opts)
 keymap.set("v", "<s-up>", ":m '<-2<cr>gv=gv", opts)
 
--- When deleting a single character, don't overwrite whatever was last yanked.
-opts.desc = "Delete character"
-keymap.set("n", "x", '"_x')
-
 -- Allow inserting blank line before/after without leaving normal mode or
 -- moving the cursor.
 opts.desc = "Add blank line below"
@@ -52,11 +62,19 @@ opts.desc = "Add blank line above"
 keymap.set("n", "<leader>O", [[:<C-u>call append(line(".")-1, repeat([""], v:count1))<cr>]], opts)
 
 -- Whole file search and replace. These allow you to simply type "old/new"
--- after.
+-- after. The "\v" part is "very magic" mode, which gives special symbols like
+-- "|", "{", etc. their normal regex syntax meaning rather than being literals.
+-- See :h magic.
 opts.desc = "Replace all in current buffer"
-keymap.set("n", "<leader>ra", ":%s//g<left><left>", opts)
+keymap.set("n", "<leader>ra", ":%s/\\v/g<left><left>", opts)
 opts.desc = "Replace with confirmation in current buffer"
-keymap.set("n", "<leader>rc", ":%s//gc<left><left>", opts)
+keymap.set("n", "<leader>rc", ":%s/\\v/gc<left><left>", opts)
+
+-- Similar to above, use "very magic" mode for searching.
+opts.desc = "Find string (forward)"
+keymap.set({ "n", "v" }, "/", "/\\v", opts)
+opts.desc = "Find string (backward)"
+keymap.set({ "n", "v" }, "?", "?\\v", opts)
 
 -- Remap for dealing with word wrap.
 keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -104,7 +122,7 @@ keymap.set("n", "<leader>zi", function()
 end, opts)
 
 opts.desc = "Remove folds"
-keymap.set("n", "<leader>zq", function()
+keymap.set("n", "<leader>zx", function()
     vim.o.foldmethod = "manual"
     vim.cmd.normal("zE")
 end, opts)
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,11 @@ return {
         config = function()
             vim.cmd.colorscheme("cybercrime")
             -- Make background transparent.
+            --
+            -- TODO: Figure out how to expose a `transparent = true` setting in
+            -- the color scheme itself, like some other popular schemes. Also,
+            -- it would be nice to have a keymap for turning transparency
+            -- on/off.
             vim.cmd([[
                 highlight Normal guibg=none
                 highlight NonText guibg=none
